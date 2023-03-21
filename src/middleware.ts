a5bf98c23c57a635742e24154a26a67314c64f13
Koki Kubota <39072144+KokiKubota2@users.noreply.github.com>
2023-03-22 07:27:22 +0900

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,15 @@ import type { NextRequest } from 'next/server'
 
 export const middleware = (request: NextRequest) => {
   const requestHeaders = new Headers(request.headers)
-  console.log(requestHeaders)
+
   requestHeaders.set('Access-Control-Allow-Origin', '*')
-  console.log(1)
-
   requestHeaders.set('Access-Control-Allow-Methods', 'POST')
-  console.log(2)
-
   requestHeaders.set('Access-Control-Allow-Headers', 'Content-Type')
-  console.log(3)
-
   requestHeaders.set('Access-Control-Allow-Credentials', 'true')
-  console.log(4)
-
   // You can also set request headers in NextResponse.rewrite
   const response = NextResponse.next({ request: { headers: requestHeaders } })
-  console.log(5)
+
+  console.log(response.headers)
 
   return response
 }
