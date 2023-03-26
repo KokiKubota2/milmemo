2d59576daf83c64e4d800908af65f7687dda6214
Koki Kubota <39072144+KokiKubota2@users.noreply.github.com>
2023-03-26 23:21:55 +0900

    auth

--- src/app/AuthProvider.tsx
+++ src/app/AuthProvider.tsx
@@ -1,25 +1,30 @@
 'use client'
 
 import { getAuth } from 'firebase/auth'
+import { getFirestore, doc } from 'firebase/firestore'
+import { useDocument } from 'react-firebase-hooks/firestore'
 import { useAuthState } from 'react-firebase-hooks/auth'
 
 import { Signin } from 'components'
 
-import firebase from 'lib/firebase'
-
 const C: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const auth = getAuth(firebase)
+  const auth = getAuth()
+  const db = getFirestore()
   const [user, loading, error] = useAuthState(auth)
+  const [userDoc, userDocLoading] = useDocument(
+    user && doc(db, `users/${user.uid}`)
+  )
 
-  if (loading) return <div>Loading...</div>
+  if (loading || userDocLoading) return <>Loading...</>
   if (error) {
     console.error(error)
-    return <div>Error</div>
+    return <>Error</>
   }
 
   console.log(user?.email, user?.uid)
 
   if (!user) return <Signin />
+  if (!userDoc?.exists()) return <>Not Allowed User</>
 
   return <>{children}</>
 }
